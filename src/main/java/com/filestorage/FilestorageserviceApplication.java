0953aec9b5bfe01521ec8c06a16aeb4c74d9f50b
saini <rsainithesh@gmail.com>
2024-11-10 16:57:56 -0500

    AWS S3 connection

---
+++ src/main/java/com/filestorage/FilestorageserviceApplication.java
@@ -0,0 +1,13 @@
+package com.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestorageserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilestorageserviceApplication.class, args);
+	}
+
+}
